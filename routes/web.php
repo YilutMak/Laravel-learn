f69f5a65a7762a0c62c05812e376d8a9bbcd6e09
YilutMak <Yilutmak@gmail.com>
2023-05-12 18:57:56 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//required parameters
+//Route::get('ID/{id}',function($id) {
+//    echo 'ID: '.$id;
+//});
+
+//optional parameters
+//Route::get('user/{name?}', function ($name = 'TutorialsPoint') {
+//    return $name;
+//});
+
+//named routes
+//Route::get('user/profile', 'UserController@showProfile')->name('profile');
+
+
+//controller routes
+//Route::get(‘base URI’,’controller@method’);
+
+Route::get('profile', [
+    'middleware' => 'auth',
+    'uses' => 'UserController@showProfile'
+]);
+
+Route::get('/usercontroller/path', [
+    'middleware' => 'First',
+    'uses' => '\App\Http\Controllers\UserController@showPath'
+]);
+
+Route::resource('my','\App\Http\Controllers\MyController');
+
+//implicit Controllers
+Route::controller(‘base URI’,’<class-name-of-the-controller>’);
